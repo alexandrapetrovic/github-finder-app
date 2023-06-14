9ed7dd0bff26853ff7e23a8cc61ec00c02e2b81a
Alexandra <aleksandraakica16@gmail.com>
2023-06-14 19:39:16 +0200

    added card component on js

--- index.js
+++ index.js
@@ -23,18 +23,101 @@ const getUser = async (username) => {
         `
         results.innerHTML = responseText;
     } else {
-       //ternarni operator ? :
+       
         const name = response.name ? response.name : 'Not provided';
-        /*if (response.name) {
-            name = response.name;
-        } else {
-            name = 'Not provided';
-        }*/
-        }
-        console.log(name)
+       
+        const username = response.login; 
+
+        const bio = response.bio ? response.bio : 'No bio';
+
+        const email = response.email ? response.email : 'No email';
+
+        const following = response.following ? response.following : '0';
+
+        const followers = response.followers ? response.followers : '0';
+
+        const publicRepos = response.public_repos ? response.public_repos : '0';
+
+        const location = response.location ? response.location : 'No location';
+
+        const blog = response.blog ? response.blog : 'No blog';
+
+        let company = response.company ? response.company : 'Not provided';
+
+        let createdAt = response.created_at; //2007-10-20T05:24:19Z
+
+        let createdAtarr = createdAt.split("T");
+
+        console.log (createdAtarr);
+
+        console.log (createdAtarr[0]); //2007-10-20
+
+        let datum = createdAtarr[0];
+
+        let datumarr = datum.split("-");
+
+        let godina = datumarr[0];
+
+        let mesec = datumarr[1];
+
+        let dan = datumarr[2];
+
+        const avatar = response.avatar_url;
+
+        const card = `
+        <h2 class="text-primary mt-8 mb-3 fw-bold">Results:</h2>
+        <article class="card">
+            <div class="row-1">
+                <img class="card-img-top" src=${avatar} alt="card image 1">
+                <div>
+                    <h2 class="fw-bold">${name}</h2>
+                    <span class="text-primary fw-bold">${username}</span>
+                </div>
+            </div>
+            <div class="row-2">
+                <div class="data">
+                    <p>${publicRepos}<br><span>Repositories</span></p>
+                    <div class="divider"></div>
+                    <p>${followers}<br><span>Followers</span></p>
+                    <p>${following}<br><span>Following</span></p>
+                </div>
+            </div>
+            <duv class="row-3 my-10">
+                <div class="d-flex">
+                    <img class="icon" src="./icons/building.png" alt="building-icon" width="18" height="20">
+                    <p class="mx-4 fw-medium">${company}</p>
+                </div>
+                <div class="d-flex">
+                    <img class="icon" src="./icons/card.png" alt="card-icon" width="18" height="20">
+                    <p class="mx-4 text-muted fw-medium">${bio}</p>
+                </div>
+                <div class="d-flex">
+                    <img class="icon" src="./icons/clock.png" alt="clock-icon" width="18" height="20">
+                    <p class="mx-4 fw-medium">Active seance ${dan}.${mesec}.${godina}</p>
+                </div>
+                <div class="d-flex">
+                    <img class="icon" src="./icons/email.png" alt="email-icon" width="18" height="20">
+                    <p class="mx-4 text-muted fw-medium">${email}</p>
+                </div>
+                <div class="d-flex">
+                    <img class="icon" src="./icons/location.png" alt="location-icon" width="18" height="20">
+                    <p class="mx-4 fw-medium">${location}</p>
+                </div>
+                <div class="d-flex">
+                    <img class="icon" src="./icons/link.png" alt="link-icon" width="18" height="20">
+                    <a class="mx-4 fw-medium" href=${blog} target="_blank">${blog}</a>
+                </div>
+                    
+            </div>
+        </article>
+        `
+        results.innerHTML=card;
     }
+
+
+} 
+
     
-}
 
 const getUsers = (username) => {
 
